0ed8b3a5d8032a85a3198019e430fb90dfd4aa14
Ester <ester@MacBook-Pro-von-Ester.local>
2026-07-08 11:53:21 +0200

    counting collectibles

--- parse_map.c
+++ parse_map.c
@@ -6,7 +6,7 @@
 /*   By: esttina <esttina@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 15:54:25 by esttina           #+#    #+#             */
-/*   Updated: 2026/07/04 03:32:04 by esttina          ###   ########.fr       */
+/*   Updated: 2026/07/04 05:28:03 by esttina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,8 +103,9 @@ void player_position(t_game *game)
             {
                 game->player_y = y;
                 game->player_x = x;
-                return ;
             }
+            else if (game->map[y][x] == 'C')
+                game->collectibles_total++;
             x++;
         }
         y++;
