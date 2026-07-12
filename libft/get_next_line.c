57a25d8c1c7ccc271a1b2ceff1a4d7fb3c127952
estertina <estert99@gmail.com>
2026-07-12 23:06:20 +0200

    norminette

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: esttina <esttina@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 12:35:00 by esttina           #+#    #+#             */
-/*   Updated: 2026/05/23 12:56:43 by esttina          ###   ########.fr       */
+/*   Updated: 2026/07/12 23:05:27 by esttina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,103 +14,103 @@
 
 static void	join(char *line, char *remainder, char *tmp)
 {
-    int	i;
-    int	c;
-    
-    i = 0;
-    c = 0;
-    while (line && line[i])
-    {
-        tmp[i] = line[i];
-        i++;
-    }
-    while (remainder[c])
-    {
-        tmp[i++] = remainder[c];
-        if (remainder[c] == '\n')
-        {
-            c++;
-            break ;
-        }
-        c++;
-    }
-    tmp[i] = '\0';
-    i = 0;
-    while (remainder[c])
-    remainder[i++] = remainder[c++];
-    remainder[i] = '\0';
+	int	i;
+	int	c;
+
+	i = 0;
+	c = 0;
+	while (line && line[i])
+	{
+		tmp[i] = line[i];
+		i++;
+	}
+	while (remainder[c])
+	{
+		tmp[i++] = remainder[c];
+		if (remainder[c] == '\n')
+		{
+			c++;
+			break ;
+		}
+		c++;
+	}
+	tmp[i] = '\0';
+	i = 0;
+	while (remainder[c])
+		remainder[i++] = remainder[c++];
+	remainder[i] = '\0';
 }
 
 static char	*join_alloc(char *line, char *remainder)
 {
-    char	*tmp;
-    int		line_len;
-    int		rem_len;
-    
-    line_len = 0;
-    rem_len = 0;
-    if (line)
-    line_len = ft_strlen(line);
-    while (remainder[rem_len] && remainder[rem_len] != '\n')
-    rem_len++;
-    if (remainder[rem_len] == '\n')
-    rem_len++;
-    tmp = malloc(line_len + rem_len + 1);
-    if (!tmp)
-    return (free(line), NULL);
-    join(line, remainder, tmp);
-    free(line);
-    return (tmp);
+	char	*tmp;
+	int		line_len;
+	int		rem_len;
+
+	line_len = 0;
+	rem_len = 0;
+	if (line)
+		line_len = ft_strlen(line);
+	while (remainder[rem_len] && remainder[rem_len] != '\n')
+		rem_len++;
+	if (remainder[rem_len] == '\n')
+		rem_len++;
+	tmp = malloc(line_len + rem_len + 1);
+	if (!tmp)
+		return (free(line), NULL);
+	join(line, remainder, tmp);
+	free(line);
+	return (tmp);
 }
 
 static char	*read_and_join(int fd, char *remainder)
 {
-    char	*line;
+	char	*line;
 	int		bytes_read;
-    
+
 	line = NULL;
 	if (ft_strchr(remainder, '\n'))
-    return (join_alloc(line, remainder));
+		return (join_alloc(line, remainder));
 	if (remainder[0] != '\0')
 	{
-        line = join_alloc(line, remainder);
+		line = join_alloc(line, remainder);
 		if (!line)
-        return (NULL);
+			return (NULL);
 	}
 	while (!line || !ft_strchr(line, '\n'))
 	{
-        bytes_read = read(fd, remainder, BUFFER_SIZE);
+		bytes_read = read(fd, remainder, BUFFER_SIZE);
 		if (bytes_read < 0)
-        return (remainder[0] = '\0', free(line), NULL);
+			return (remainder[0] = '\0', free(line), NULL);
 		if (bytes_read == 0)
-        break ;
+			break ;
 		remainder[bytes_read] = '\0';
 		line = join_alloc(line, remainder);
 		if (!line)
-        return (NULL);
+			return (NULL);
 	}
 	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-    static char	remainder[MAX_FD][BUFFER_SIZE + 1];
-    char		*line;
+	static char	remainder[MAX_FD][BUFFER_SIZE + 1];
+	char		*line;
 
-    if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0)
-        return (NULL);
-    line = read_and_join(fd, remainder[fd]);
-    if (!line)
-        remainder[fd][0] = '\0';
-    return (line);
+	if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = read_and_join(fd, remainder[fd]);
+	if (!line)
+		remainder[fd][0] = '\0';
+	return (line);
 }
 
 // int	main(void)
 // {
-    // 	int		fd1, fd2, fd3;
-    // 	char	*line
-    // 	printf("\nROUND 1\n");
-    
+// 	int		fd1, fd2, fd3;
+// 	char	*line
+// 	printf("\nROUND 1\n");
+
 // 	line = get_next_line(fd1);
 // 		printf("FD %d: %s", fd1, line);
 // 	free(line);
